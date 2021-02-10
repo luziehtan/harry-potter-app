623f2ae9e46f2c916050578947c7167fdf0409ac
Luciana Blümlein <luziehtan@hotmail.com>
2021-02-10 16:20:12 +0100

    Add card style and fontawesome, update base.css, variables.css, global.css

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,6 +1,29 @@
 import './Card.css'
 import createElement from '../../lib/createElement'
 
-export default function Card(name) {
-  return createElement('section', { className: 'Card' }, name)
+export default function Card(image, name, house) {
+  const Card = createElement(
+    'section',
+    { className: 'Card' },
+    image,
+    name,
+    house
+  )
+  Card.style.backgroundColor = getColorByHouse(house)
+  Card.style.color = 'var(--font-color)'
+  Card.innerHTML = `
+  <img class="characterimg" src="${image}">
+  <h2>${name}</h2>
+  <span class="housename">${house}</span>
+ `
+
+  function getColorByHouse(house) {
+    if (house === 'Gryffindor') return 'var(--color-gryffindor)'
+    if (house === 'Slytherin') return 'var(--color-slytherin)'
+    if (house === 'Hufflepuff') return 'var(--color-hufflepuff)'
+    if (house === 'Ravenclaw') return 'var(--color-ravenclaw)'
+    if (house === '') return 'var(--color-black)'
+  }
+
+  return Card
 }
